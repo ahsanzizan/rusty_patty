7dc46cb5faf33ecf52c14110714018535e7cd648
Ahsan <ahsanaz461@gmail.com>
2023-12-10 19:53:58 +0700

    Separate commands from the main file

---
+++ src/commands.rs
@@ -0,0 +1,96 @@
+use std::{io, env};
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+// Function to handle the "cd" command
+pub fn change_directory(args: &mut Vec<&str>) {
+    // Extract the new directory from the arguments
+    let new_dir: &str = args.iter().peekable().peek().map_or("/", |x| **x);
+    let root: &Path = Path::new(new_dir);
+
+    // Attempt to change the current directory
+    if let Err(e) = env::set_current_dir(&root) {
+        eprintln!("{}", e);
+    }
+}
+
+// Function to handle the "dir" command
+pub fn list_directory() {
+    // Get the current directory
+    let current_dir: std::path::PathBuf = env::current_dir().unwrap();
+
+    // Attempt to list entries in the directory
+    if let Ok(entries) = current_dir.read_dir() {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                println!("{}", entry.file_name().to_string_lossy());
+            }
+        }
+    } else {
+        eprintln!("Error listing directory");
+    }
+}
+
+// Function to handle the "echo" command
+pub fn echo(args: std::str::SplitWhitespace<'_>) {
+    // Concatenate the arguments and print the output
+    let output: String = args.collect::<Vec<&str>>().join(" ");
+    println!("{}", output);
+}
+
+// Function to handle the "rm" command
+pub fn rm(args: std::str::SplitWhitespace<'_>) {
+    // Extract the file path from the args
+    let file_path: Option<&str> = args.peekable().peek().map(|x| *x);
+
+    // Check if a file path is provided
+    match file_path {
+        Some(path) => {
+            if let Err(e) = std::fs::remove_file(path) {
+                eprintln!("{}", e);
+            }
+        }
+        None => {
+            eprintln!("Usage: rm <file_path>");
+        }
+    }
+}
+
+// Function to execute an external command
+pub fn execute_external_command(
+    command: &str,
+    args: std::str::SplitWhitespace<'_>,
+    stdin: Stdio,
+    stdout: Stdio,
+) -> Option<std::process::Child> {
+    // Spawn the external command process
+    let output: Result<std::process::Child, io::Error> = Command::new(command)
+        .args(args)
+        .stdin(stdin)
+        .stdout(stdout)
+        .spawn();
+
+    // Handle the result of spawning the process
+    match output {
+        Ok(output) => Some(output),
+        Err(e) => {
+            // Print an error message and return None on error
+            eprintln!("{}", e);
+            None
+        }
+    }
+}
+
+// Function to display help information
+pub fn display_help() {
+    println!("Available commands:");
+    println!("  cd <directory>   - Change current directory");
+    println!("  dir              - List entries in the current directory");
+    println!("  echo <message>   - Print a message to the console");
+    println!("  rm <file_path>   - Remove a file");
+    println!("  exit             - Exit the shell");
+    println!("  <command>        - Execute an external command");
+
+    println!("Usage:");
+    println!("  Multiple commands can be chained using ' | ' for piping.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,7 @@
-use std::env;
 use std::io::{self, Write};
-use std::path::Path;
-use std::process::{Command, Stdio};
+use std::process::Stdio;
 
-// Function to handle the "cd" command
-fn change_directory(args: &mut Vec<&str>) {
-    // Extract the new directory from the arguments
-    let new_dir: &str = args.iter().peekable().peek().map_or("/", |x| **x);
-    let root: &Path = Path::new(new_dir);
-
-    // Attempt to change the current directory
-    if let Err(e) = env::set_current_dir(&root) {
-        eprintln!("{}", e);
-    }
-}
-
-// Function to handle the "dir" command
-fn list_directory() {
-    // Get the current directory
-    let current_dir: std::path::PathBuf = env::current_dir().unwrap();
-
-    // Attempt to list entries in the directory
-    if let Ok(entries) = current_dir.read_dir() {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                println!("{}", entry.file_name().to_string_lossy());
-            }
-        }
-    } else {
-        eprintln!("Error listing directory");
-    }
-}
-
-// Function to handle the "echo" command
-fn echo(args: std::str::SplitWhitespace<'_>) {
-    // Concatenate the arguments and print the output
-    let output: String = args.collect::<Vec<&str>>().join(" ");
-    println!("{}", output);
-}
-
-// Function to handle the "rm" command
-fn rm(args: std::str::SplitWhitespace<'_>) {
-    // Extract the file path from the args
-    let file_path: Option<&str> = args.peekable().peek().map(|x| *x);
-
-    // Check if a file path is provided
-    match file_path {
-        Some(path) => {
-            if let Err(e) = std::fs::remove_file(path) {
-                eprintln!("{}", e);
-            }
-        }
-        None => {
-            eprintln!("Usage: rm <file_path>");
-        }
-    }
-}
-
-// Function to execute an external command
-fn execute_external_command(
-    command: &str,
-    args: std::str::SplitWhitespace<'_>,
-    stdin: Stdio,
-    stdout: Stdio,
-) -> Option<std::process::Child> {
-    // Spawn the external command process
-    let output: Result<std::process::Child, io::Error> = Command::new(command)
-        .args(args)
-        .stdin(stdin)
-        .stdout(stdout)
-        .spawn();
-
-    // Handle the result of spawning the process
-    match output {
-        Ok(output) => Some(output),
-        Err(e) => {
-            // Print an error message and return None on error
-            eprintln!("{}", e);
-            None
-        }
-    }
-}
-
-// Function to display help information
-fn display_help() {
-    println!("Available commands:");
-    println!("  cd <directory>   - Change current directory");
-    println!("  dir              - List entries in the current directory");
-    println!("  echo <message>   - Print a message to the console");
-    println!("  rm <file_path>   - Remove a file");
-    println!("  exit             - Exit the shell");
-    println!("  <command>        - Execute an external command");
-
-    println!("Usage:");
-    println!("  Multiple commands can be chained using ' | ' for piping.");
-}
+mod commands;
 
 // Main REPL loop
 fn main() {
@@ -116,23 +23,23 @@ fn main() {
 
             match command {
                 "help" => {
-                    display_help();
+                    commands::display_help();
                     previous_command = None;
                 }
                 "cd" => {
-                    change_directory(&mut args.collect::<Vec<&str>>());
+                    commands::change_directory(&mut args.collect::<Vec<&str>>());
                     previous_command = None;
                 }
                 "dir" => {
-                    list_directory();
+                    commands::list_directory();
                     previous_command = None;
                 }
                 "echo" => {
-                    echo(args);
+                    commands::echo(args);
                     previous_command = None;
                 }
                 "rm" => {
-                    rm(args);
+                    commands::rm(args);
                     previous_command = None;
                 }
                 "exit" => return,
@@ -148,7 +55,9 @@ fn main() {
                         Stdio::inherit()
                     };
 
-                    if let Some(output) = execute_external_command(command, args, stdin, stdout) {
+                    if let Some(output) =
+                        commands::execute_external_command(command, args, stdin, stdout)
+                    {
                         previous_command = Some(output);
                     } else {
                         previous_command = None;
