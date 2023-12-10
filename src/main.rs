fd71aa37ccec30ac811e7dab4a10d42e7134cce0
Ahsan <ahsanaz461@gmail.com>
2023-12-10 16:52:16 +0700

    Refactor the code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 use std::io::{self, Write};
 use std::process::{exit, Command, Stdio};
 
+fn execute_command(command: &str, args: Vec<&str>, input: Option<Vec<u8>>) -> Vec<u8> {
+    let mut child: std::process::Child = Command::new(command)
+        .args(&args)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn command");
+
+    if let Some(input_data) = input {
+        if let Some(ref mut child_stdin) = child.stdin.take() {
+            child_stdin
+                .write_all(&input_data)
+                .expect("Failed to write to child stdin");
+        }
+    }
+
+    let output: std::process::Output = child
+        .wait_with_output()
+        .expect("Failed to wait for command");
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    if !output.stderr.is_empty() {
+        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    output.stdout
+}
+
 fn main() {
     let mut previous_output: Option<Vec<u8>> = None;
 
@@ -18,55 +46,23 @@ fn main() {
         }
 
         if input.contains('|') {
-            let mut commands: std::iter::Map<std::str::Split<'_, &str>, fn(&str) -> &str> =
-                input.split("|").map(str::trim);
-            while let Some(command) = commands.next() {
+            let commands: Vec<&str> = input.split('|').map(str::trim).collect();
+
+            for command in commands {
                 let parts: Vec<&str> = command.split_whitespace().collect();
-                let mut child: std::process::Child = Command::new(parts[0])
-                    .args(&parts[1..])
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .expect("Failed to spawn command in pipeline");
-
-                if let Some(prev_output) = previous_output.take() {
-                    if let Some(ref mut child_stdin) = child.stdin.take() {
-                        child_stdin
-                            .write_all(&prev_output)
-                            .expect("Failed to write to child stdin");
-                    }
-                }
-
-                let output: std::process::Output = child
-                    .wait_with_output()
-                    .expect("Failed to wait for command in pipeline");
-
-                println!("{}", String::from_utf8_lossy(&output.stdout));
-                if !output.stderr.is_empty() {
-                    eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-                }
-
-                previous_output = Some(output.stdout);
+                previous_output = Some(execute_command(
+                    parts[0],
+                    parts[1..].to_vec(),
+                    previous_output.take(),
+                ));
             }
-            continue;
+        } else {
+            let parts: Vec<&str> = input.split_whitespace().collect();
+            previous_output = Some(execute_command(
+                parts[0],
+                parts[1..].to_vec(),
+                previous_output.take(),
+            ));
         }
-
-        let mut parts: std::str::SplitWhitespace<'_> = input.split_whitespace();
-        let command: &str = parts.next().unwrap();
-        let args: Vec<&str> = parts.collect();
-
-        let output: std::process::Output = Command::new(command)
-            .args(&args)
-            .output()
-            .expect("Failed to execute command");
-
-        println!("{}", String::from_utf8_lossy(&output.stdout));
-
-        if !output.stderr.is_empty() {
-            eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-        }
-
-        // Update previous_output for non-pipeline commands
-        previous_output = Some(output.stdout);
     }
 }
