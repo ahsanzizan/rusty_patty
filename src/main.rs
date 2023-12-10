b0a30eaa11f427905985351de67d555ef9157d3a
Ahsan <ahsanaz461@gmail.com>
2023-12-10 18:49:05 +0700

    Don't use Windows cmd

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,81 @@
-use std::fs::File;
+use std::env;
 use std::io::{self, Write};
-use std::process::{exit, Command, Stdio};
-
-fn execute_command(command: &str, args: Vec<&str>, input: Option<Vec<u8>>) -> Vec<u8> {
-    let mut child: std::process::Child = Command::new("cmd")
-        .arg("/C")
-        .arg(command)
-        .args(&args)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn command");
-
-    if let Some(input_data) = input {
-        if let Some(ref mut child_stdin) = child.stdin.take() {
-            child_stdin
-                .write_all(&input_data)
-                .expect("Failed to write to child stdin");
-        }
-    }
-
-    let output: std::process::Output = child
-        .wait_with_output()
-        .expect("Failed to wait for command");
-
-    println!("{}", String::from_utf8_lossy(&output.stdout));
-    if !output.stderr.is_empty() {
-        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-    }
-
-    output.stdout
-}
+use std::path::Path;
+use std::process::{Command, Stdio};
 
 fn main() {
-    let mut previous_output: Option<Vec<u8>> = None;
-
+    // Main REPL loop
     loop {
         print!("> ");
         io::stdout().flush().unwrap();
 
-        let mut input: String = String::new();
+        // Read user input
+        let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let input: &str = input.trim();
+        // Split input into commands separated by pipe ('|') and make it peekable
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        if input == "exit" {
-            exit(0);
+        // Process each command
+        while let Some(command) = commands.next() {
+            // Split command into command and arguments
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            // Match the command type
+            match command {
+                "cd" => {
+                    // Change directory command
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    // Reset previous command since 'cd' doesn't produce output
+                    previous_command = None;
+                }
+                "exit" => return, // Exit the REPL
+                command => {
+                    // External command
+                    let stdin: Stdio = previous_command
+                        .map_or(Stdio::inherit(), |output: std::process::Child| {
+                            Stdio::from(output.stdout.unwrap())
+                        });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    // Spawn the command process
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            // Update previous command with the new process
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            // Reset previous command on error
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
+                }
+            }
         }
 
-        if input.contains('|') {
-            let commands: Vec<&str> = input.split('|').map(str::trim).collect();
-
-            for command in commands {
-                let parts: Vec<&str> = command.split_whitespace().collect();
-                previous_output = Some(execute_command(
-                    parts[0],
-                    parts[1..].to_vec(),
-                    previous_output.take(),
-                ));
-            }
-        } else if input.contains(">>") {
-            let mut parts: std::iter::Map<std::str::Split<'_, &str>, fn(&str) -> &str> =
-                input.split(">>").map(str::trim);
-            let command: &str = parts.next().unwrap();
-            let filename: &str = parts.next().expect("Missing filename after '>>'");
-            let output: Vec<u8> = execute_command(command, vec![], previous_output.take());
-
-            let mut file: File = File::create(filename).expect("Failed to create file");
-            file.write_all(&output).expect("Failed to write to file");
-        } else if input.contains('>') {
-            let mut parts: std::iter::Map<std::str::Split<'_, char>, fn(&str) -> &str> =
-                input.split('>').map(str::trim);
-            let command: &str = parts.next().unwrap();
-            let filename: &str = parts.next().expect("Missing filename after '>");
-            let output: Vec<u8> = execute_command(command, vec![], previous_output.take());
-
-            let mut file: File = File::create(filename).expect("Failed to create file");
-            file.write_all(&output).expect("Failed to write to file");
-        } else if input.contains('<') {
-            let mut parts: std::iter::Map<std::str::Split<'_, char>, fn(&str) -> &str> =
-                input.split('<').map(str::trim);
-            let command: &str = parts.next().unwrap();
-            let filename: &str = parts.next().expect("Missing filename after '<'");
-            let input_data: Vec<u8> = std::fs::read(filename).expect("Failed to read from file");
-            previous_output = Some(execute_command(command, vec![], Some(input_data)));
-        } else {
-            let parts: Vec<&str> = input.split_whitespace().collect();
-            previous_output = Some(execute_command(
-                parts[0],
-                parts[1..].to_vec(),
-                previous_output.take(),
-            ));
+        // Wait for the final command to finish before prompting for new input
+        if let Some(mut final_command) = previous_command {
+            final_command.wait().unwrap();
         }
     }
 }
